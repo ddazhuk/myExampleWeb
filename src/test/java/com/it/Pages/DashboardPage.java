62c45883e54b9dd43de3e8af6102eb88fd5ba172
ddazhuk <ddazhuk@gmail.com>
2018-06-17 12:41:54 +0300

    commit 1

--- src/test/java/com/it/Pages/DashboardPage.java
+++ src/test/java/com/it/Pages/DashboardPage.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DashboardPage  extends  BasePage{
 	@FindBy(xpath = "//span[@class='sn_menu_title']")
 	private WebElement userEmail;
 
 	public String getUserEmail() {
+		WebDriverWait wait = new WebDriverWait(driver,10);
+		wait.until(ExpectedConditions.visibilityOf(userEmail));
 		return  userEmail.getText();
 	}
 
